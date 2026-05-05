23b1d91cd55baab879c8ebb253fc89dbbbfe8afe
Zhenya <zhenek.dem@gmail.com>
2026-05-05 13:56:25 +0300

    Исправила ошибки

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -3,11 +3,11 @@ let min = Infinity;
 let max = -Infinity;
 let sum = 0;
 
-for (let i = 0; i < arr.length; i++)
+for (let i = 0; i < arr.length; i++) {
   if (arr[i] > max) max = arr[i];
   if (arr[i] < min) min = arr[i];
   sum += arr[i];
-
+}
   const avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
@@ -29,6 +29,7 @@ return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
+if (arr.length === 0) return 0;
 let sumEvenElement = 0;
 let sumOddElement = 0;
 for (let i = 0; i < arr.length; i++) {
